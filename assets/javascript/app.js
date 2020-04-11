f34f0b1763efb43f3fc5bbf6d6f83b55ab3ebff4
Christina Lupanow <celupanow@gmail.com>
2020-04-10 23:29:05 -0600

    updated HTML and CSS to have basic styling, added a js file to demonstrate changing the CSS on a button click

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+
+
+
+// when a button is clicked, the css changes - this is just a simple example
+
+$(".dad").on("click", function() {
+    console.log("click is working");
+    $("#results").css("background-color", "white");
+});
